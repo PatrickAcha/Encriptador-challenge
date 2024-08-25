3f3c514e86efe6f1c9df3358635b614e2dadae5c
PatrickAcha <patrickquispeacha@gmail.com>
2024-08-25 10:21:07 -0500

    versión encriptación

---
+++ script.js
@@ -0,0 +1,102 @@
+var botonEncriptar = document.querySelector(".btn-encriptar");
+var botonDesencriptar = document.querySelector(".btn-desencriptar");
+var munieco = document.querySelector(".contenedormunieco");
+var contenedor = document.querySelector(".contenedor-parrafo");
+var resultado = document.querySelector(".texto-resultado");
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+
+function encriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = encriptarTexto(cajatexto);
+}
+
+function desencriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = desencriptarTexto(cajatexto);
+}
+
+function recuperarTexto(){
+    var cajatexto = document.querySelector(".cajatexto")
+    return cajatexto.value
+}
+
+function ocultarAdelante(){
+    munieco.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+
+}
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    texto = texto.replace(/ai/g, "a");
+    texto = texto.replace(/enter/g, "e");
+    texto = texto.replace(/imes/g, "i");
+    texto = texto.replace(/ober/g, "o");
+    texto = texto.replace(/ufat/g, "u");
+    
+    return texto;
+}
+const btnCopiar = document.querySelector(".btn-copiar");
+
+btnCopiar.addEventListener("click", function() {
+    var contenido = document.querySelector(".texto-resultado").textContent;
+
+    if (contenido) {
+        navigator.clipboard.writeText(contenido)
+            .then(() => {
+                console.log("Texto copiado exitosamente: " + contenido);
+            })
+            .catch(err => {
+                console.error("Error al copiar el texto: ", err);
+            });
+    } else {
+        console.log("No hay texto para copiar");
+    }
+});
+
+// Selecciona el elemento del tren
+const train = document.querySelector('.train');
+
+// Función para mover el tren
+function moveTrain() {
+    // Mueve el tren hacia la derecha
+    train.style.left = '200px';
+
+    // Espera hasta que la animación termine y luego reinicia la posición
+    setTimeout(() => {
+        train.style.left = '0';
+    }, 4000); // Tiempo debe coincidir con la duración de la transición en CSS (4s)
+}
+
+// Llama a la función repetidamente con un intervalo
+setInterval(moveTrain, 4100); 
